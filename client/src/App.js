092a1b697c948f4e519ddf8786103720a9b4c575
Ana Mari <anitamari.1979@gmail.com>
2023-02-21 17:30:14 +0100

    app with components

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+//import React, { useState } from "react";
+// // import NewResolutionForm from "./components/NewResolutionForm";
+// // import ResolutionList from "./components/ResolutionList";
 import './App.css';
 
 function App() {
---
+++ client/src/components/NewResolutionForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./NewResolutionForm.css";
+
+
+
+
+
+
+
+
+
+export default NewResolutionForm;
