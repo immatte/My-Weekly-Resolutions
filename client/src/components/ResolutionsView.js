f0af65a13dfbe3ed2bd9b9a42b40b697962cf0ef
immatte <matte_isabelle@yahoo.fr>
2023-03-07 22:41:55 +0100

    Updated

--- client/src/components/ResolutionsView.js
+++ client/src/components/ResolutionsView.js
@@ -13,16 +13,17 @@ export default function ResolutionsView(props) {
         <div className="myresolutionList">
                 <ul className="myresolList">
                     {props.resolutions.map(r => (
-                        //check if possible to add the day weeks as references
-                        //check css for completed
+                        //adding a link to the DayViews
                             <li id="myresolItems" key={r.id} className={r.complete ? "complete" : null}>
                             <label >
+                                    {/* toggleDoneCb button not working */}
                                     <button type="text" className="bi bi-check-square-fill" onClick={e => props.toggleDoneCb(r.id)}>
                                     ✔
                                     </button>
                                 </label >
                                 {r.text }
                                 <label>
+                                    {/* deleteCb button working */}
                                     <button type="text" className="btn btn-danger" onClick={e => props.deleteCb(r.id)}>
                                         X
                                     </button>
